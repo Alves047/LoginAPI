a8462e1b694f38edf0bf4f6f86c8bc82169bd5d2
DevG4L0 <85362752+G4L04@users.noreply.github.com>
2022-03-15 00:23:21 -0300

    Add files via upload

---
+++ admRouter.js
@@ -0,0 +1,24 @@
+// Variables Imports
+const express = require("express");
+const router = express.Router();
+const auth = require("../controllers/authControll");
+
+//Access Routes
+router.get("/", auth, (req, res) => {
+  
+  if (req.user.admin) {
+    res.send("This data only access by admin");
+  } else {
+    res.status(401).send("Access Denied: Not admin");
+  }
+
+});
+
+//Access Free Route
+router.get("/free", auth, (req, res) => {
+  
+  res.send("This data only access by logged user");
+
+});
+
+module.exports = router;
---
+++ authControll.js
@@ -0,0 +1,19 @@
+//Variables Imports
+const jwt = require("jsonwebtoken");
+// --------------------------------------------------------------
+// Auth-Token Verify
+module.exports = function (req, res, next) {
+  const token = req.header("authorization-token");
+
+  if (!token) {
+    return res.status(401).send("Access Denied");
+  }
+
+  try {
+    const userVerified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = userVerified;
+    next();
+  } catch (err) {
+    res.status(401).send("Access Denied: Invalid token");
+  }
+};
